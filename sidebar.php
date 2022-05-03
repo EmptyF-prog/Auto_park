68a70fc12105b3083fb79f120573d7da945ed5e9
EmptyF-prog <gammer-gammer@mail.ru>
2022-05-03 18:52:16 +0300

    Доработка сайдбара
    +Полностью функциональный сайдбар
    + Рабочие метеки
    + Создан дополнительный виджет для скачивания файла

--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,3 @@
 <aside class="col-lg-4">
     <?php if ( ! dynamic_sidebar('sidebar-blog') ) :  dynamic_sidebar( 'sidebar-blog' );  endif; ?> 
-            <div class="col-lg-12">
-                <div class="sidebar-widget tag">
-                    <a href="#">web</a>
-                    <a href="#">development</a>
-                    <a href="#">seo</a>
-                    <a href="#">marketing</a>
-                    <a href="#">branding</a>
-                    <a href="#">web deisgn</a>
-                    <a href="#">Tutorial</a>
-                    <a href="#">Tips</a>
-                    <a href="#">Design trend</a>
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="sidebar-widget download">
-                    <h5 class="mb-4">Полезные файлы</h5>
-                    <a href="#"> <i class="fa fa-file-pdf"></i>Презентация Promodise</a>
-                    <a href="#"> <i class="fa fa-file-pdf"></i>10 источников бесплатного SEO</a>
-                </div>
-            </div></aside>   
+    </aside>   
