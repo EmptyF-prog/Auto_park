d934d26322401c53fd3d06a29eb0f7c255d6d93a
EmptyF-prog <gammer-gammer@mail.ru>
2022-05-13 14:33:58 +0300

    Коменты
    Возможность комментировать

--- comments.php
+++ comments.php
@@ -1,37 +1,61 @@
+<?php
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="comments my-4">
+
+	<?php
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) :
+		?>
+		<h3 class="mb-5">Комментарии:</h3>
+
+
+		<?php the_comments_navigation(); ?>
+
+		<ol class="comment-list p-0">
+			<?php
+			wp_list_comments(
+				array(
+					'walker'            => new Bootstrap_Walker_Comment(), // Какой шаблон использовать для комментариев
+                    'max_depth'         => '5',  // Максимальная вложенность 
+                    'style'             => 'ol', // Во что оборачиваются комментарии
+                    'type'              => 'all', 
+                    'reply_text'        => __('Ответить <i class="fa fa-reply"></i> '), 
+                    'per_page'          => '5',
+                    'avatar_size'       => 80,
+                    'format'            => 'html5', // или xhtml, если HTML5 не поддерживается темой
+                    'echo'              => true,     // true или false
+				)
+			);
+			?>
+		</ol><!-- .comment-list -->
+
+		<?php
+		the_comments_navigation();
+
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() ) :
+			?>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'fsdfsd' ); ?></p>
+			<?php
+		endif;
+
+	endif; // Check for have_comments().
+
+	comment_form();
+	?>
+
+</div><!-- #comments -->
+
+
+
+
+
 <div class="comments my-4">
-    <h3 class="mb-5">Комментарии:</h3>
-
-    <div class="media mb-4">
-        <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-        <div class="media-body">
-            <h5>Антон Колесников</h5>
-            <span class="text-muted">20 января 2020</span>
-            <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
-
-            <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-
-                <div class="media mt-5">
-                    <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-                    <div class="media-body">
-                        <h5>Егор Савицкий</h5>
-                        <span class="text-muted">20 января 2020</span>
-                        <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
-
-                        <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-                    </div>
-                </div>
-        </div>
-    </div>
-    <div class="media mb-4">
-        <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-        <div class="media-body">
-            <h5>Валентин Крашков</h5>
-            <span class="text-muted">14 февраля 2020</span>
-            <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
-
-            <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-        </div>
-    </div>
+ 
 </div>
 
 <div class="mt-5 mb-3">
--- functions.php
+++ functions.php
@@ -11,6 +11,16 @@ if (! function_exists('auto_park_setup'))  {
 			'header-text' => '',
 			'unlink-homepage-logo' => false, 
 		]);
+		// Подключаем поддержку HTML5 тегов
+		add_theme_support( 'html5', array(
+			'comment-list',
+			'comment-form',
+			'search-form',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		) );
 		// Добавляем динамический <title>
 		add_theme_support('title-tag');
         // Включаем миниатюры для постов и страниц	
@@ -322,3 +332,337 @@ function register_download_widget() {
 }
 add_action( 'widgets_init', 'register_download_widget' );
 
+
+// Шаблон комментов 
+
+
+class Bootstrap_Walker_Comment extends Walker {
+
+
+	public $db_fields = array(
+		'parent' => 'comment_parent',
+		'id'     => 'comment_ID',
+	);
+
+
+
+
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$GLOBALS['comment_depth'] = $depth + 1;
+
+		switch ( $args['style'] ) {
+			case 'div':
+				break;
+			case 'ol':
+				$output .= '<ol class="children">' . "\n";
+				break;
+			case 'ul':
+			default:
+				$output .= '<ul class="children">' . "\n";
+				break;
+		}
+	}
+
+
+	public function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$GLOBALS['comment_depth'] = $depth + 1;
+
+		switch ( $args['style'] ) {
+			case 'div':
+				break;
+			case 'ol':
+				$output .= "</ol><!-- .children -->\n";
+				break;
+			case 'ul':
+			default:
+				$output .= "</ul><!-- .children -->\n";
+				break;
+		}
+	}
+
+
+	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+		if ( ! $element ) {
+			return;
+		}
+
+		$id_field = $this->db_fields['id'];
+		$id       = $element->$id_field;
+
+		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+
+	
+		if ( $max_depth <= $depth + 1 && isset( $children_elements[ $id ] ) ) {
+			foreach ( $children_elements[ $id ] as $child ) {
+				$this->display_element( $child, $children_elements, $max_depth, $depth, $args, $output );
+			}
+
+			unset( $children_elements[ $id ] );
+		}
+
+	}
+
+
+	public function start_el( &$output, $data_object, $depth = 0, $args = array(), $current_object_id = 0 ) {
+		// Restores the more descriptive, specific name for use within this method.
+		$comment = $data_object;
+
+		$depth++;
+		$GLOBALS['comment_depth'] = $depth;
+		$GLOBALS['comment']       = $comment;
+
+		if ( ! empty( $args['callback'] ) ) {
+			ob_start();
+			call_user_func( $args['callback'], $comment, $args, $depth );
+			$output .= ob_get_clean();
+			return;
+		}
+
+		if ( 'comment' === $comment->comment_type ) {
+			add_filter( 'comment_text', array( $this, 'filter_comment_text' ), 40, 2 );
+		}
+
+		if ( ( 'pingback' === $comment->comment_type || 'trackback' === $comment->comment_type ) && $args['short_ping'] ) {
+			ob_start();
+			$this->ping( $comment, $depth, $args );
+			$output .= ob_get_clean();
+		} elseif ( 'html5' === $args['format'] ) {
+			ob_start();
+			$this->html5_comment( $comment, $depth, $args );
+			$output .= ob_get_clean();
+		} else {
+			ob_start();
+			$this->comment( $comment, $depth, $args );
+			$output .= ob_get_clean();
+		}
+
+		if ( 'comment' === $comment->comment_type ) {
+			remove_filter( 'comment_text', array( $this, 'filter_comment_text' ), 40 );
+		}
+	}
+
+	public function end_el( &$output, $data_object, $depth = 0, $args = array() ) {
+		if ( ! empty( $args['end-callback'] ) ) {
+			ob_start();
+			call_user_func(
+				$args['end-callback'],
+				$data_object, // The current comment object.
+				$args,
+				$depth
+			);
+			$output .= ob_get_clean();
+			return;
+		}
+		if ( 'div' === $args['style'] ) {
+			$output .= "</div><!-- #comment-## -->\n";
+		} else {
+			$output .= "</li><!-- #comment-## -->\n";
+		}
+	}
+
+	
+	protected function ping( $comment, $depth, $args ) {
+		$tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
+		?>
+		<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( '', $comment ); ?>>
+			<div class="comment-body">
+				<?php _e( 'Pingback:' ); ?> <?php comment_author_link( $comment ); ?> <?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
+			</div>
+		<?php
+	}
+
+	
+	public function filter_comment_text( $comment_text, $comment ) {
+		$commenter          = wp_get_current_commenter();
+		$show_pending_links = ! empty( $commenter['comment_author'] );
+
+		if ( $comment && '0' == $comment->comment_approved && ! $show_pending_links ) {
+			$comment_text = wp_kses( $comment_text, array() );
+		}
+
+		return $comment_text;
+	}
+
+	
+	protected function comment( $comment, $depth, $args ) {
+		if ( 'div' === $args['style'] ) {
+			$tag       = 'div';
+			$add_below = 'comment';
+		} else {
+			$tag       = 'li';
+			$add_below = 'div-comment';
+		}
+
+		$commenter          = wp_get_current_commenter();
+		$show_pending_links = isset( $commenter['comment_author'] ) && $commenter['comment_author'];
+
+		if ( $commenter['comment_author_email'] ) {
+			$moderation_note = __( 'Your comment is awaiting moderation.' );
+		} else {
+			$moderation_note = __( 'Your comment is awaiting moderation. This is a preview; your comment will be visible after it has been approved.' );
+		}
+		?>
+		<<?php echo $tag; ?> <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?> id="comment-<?php comment_ID(); ?>">
+		<?php if ( 'div' !== $args['style'] ) : ?>
+		<div id="div-comment-<?php comment_ID(); ?>" class="comment-body">
+		<?php endif; ?>
+		<div class="comment-author vcard">
+			<?php
+			if ( 0 != $args['avatar_size'] ) {
+				echo get_avatar( $comment, $args['avatar_size'] );
+			}
+			?>
+			<?php
+			$comment_author = get_comment_author_link( $comment );
+
+			if ( '0' == $comment->comment_approved && ! $show_pending_links ) {
+				$comment_author = get_comment_author( $comment );
+			}
+
+			printf(
+				/* translators: %s: Comment author link. */
+				__( '%s <span class="says">says:</span>' ),
+				sprintf( '<cite class="fn">%s</cite>', $comment_author )
+			);
+			?>
+		</div>
+		<?php if ( '0' == $comment->comment_approved ) : ?>
+		<em class="comment-awaiting-moderation"><?php echo $moderation_note; ?></em>
+		<br />
+		<?php endif; ?>
+
+		<div class="comment-meta commentmetadata">
+			<?php
+			printf(
+				'<a href="%s">%s</a>',
+				esc_url( get_comment_link( $comment, $args ) ),
+				sprintf(
+					/* translators: 1: Comment date, 2: Comment time. */
+					__( '%1$s at %2$s' ),
+					get_comment_date( '', $comment ),
+					get_comment_time()
+				)
+			);
+
+			edit_comment_link( __( '(Edit)' ), ' &nbsp;&nbsp;', '' );
+			?>
+		</div>
+
+		<?php
+		comment_text(
+			$comment,
+			array_merge(
+				$args,
+				array(
+					'add_below' => $add_below,
+					'depth'     => $depth,
+					'max_depth' => $args['max_depth'],
+				)
+			)
+		);
+		?>
+
+		<?php
+		comment_reply_link(
+			array_merge(
+				$args,
+				array(
+					'add_below' => $add_below,
+					'depth'     => $depth,
+					'max_depth' => $args['max_depth'],
+					'before'    => '<div class="reply">',
+					'after'     => '</div>',
+				)
+			)
+		);
+		?>
+
+		<?php if ( 'div' !== $args['style'] ) : ?>
+		</div>
+		<?php endif; ?>
+		<?php
+	}
+
+	
+	protected function html5_comment( $comment, $depth, $args ) {
+		$tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
+
+		$commenter          = wp_get_current_commenter();
+		$show_pending_links = ! empty( $commenter['comment_author'] );
+
+		if ( $commenter['comment_author_email'] ) {
+			$moderation_note = __( 'Ваш коментарии ожидает модерации' );
+		} else {
+			$moderation_note = __( 'Ваш коментарии ожидает модерации.  Ваш коментарии будет опубликован после проверки' );
+		}
+		?>
+
+		<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
+			<article id="div-comment-<?php comment_ID(); ?>" class="media mb-4">
+			<?php
+				if ( 0 != $args['avatar_size'] ) {
+					echo get_avatar( $comment, $args['avatar_size'], 'mystery','', array('class' => 'img-fluid d-flex mr-4 rounded') );
+				}
+				?>
+
+				<footer>					
+					<?php
+					$comment_author = get_comment_author_link( $comment );
+
+					if ( '0' == $comment->comment_approved && ! $show_pending_links ) {
+						$comment_author = get_comment_author( $comment );
+					}
+
+					printf(
+						/* translators: %s: Comment author link. */
+						__( '%s ' ),sprintf( '<h5>%s</h5>', $comment_author )
+					);
+					?>
+
+
+					<div class="comment-metadata">
+						<?php
+						printf(
+							'<a href="%s" class="text-muted"><time datetime="%s">%s</time></a>',
+							esc_url( get_comment_link( $comment, $args ) ),
+							get_comment_time( 'c' ),
+							sprintf(
+								/* translators: 1: Comment date, 2: Comment time. */
+								__( '%1$s at %2$s' ),
+								get_comment_date( 'j F Y', $comment ),
+								get_comment_time()
+							)
+						);
+
+						edit_comment_link( __( 'Edit' ), ' <span class="edit-link">', '</span>' );
+						?>
+					</div><!-- .comment-metadata -->
+
+					<?php if ( '0' == $comment->comment_approved ) : ?>
+					<em class="comment-awaiting-moderation"><?php echo $moderation_note; ?></em>
+					<?php endif; ?>
+				<div class="mt-2">
+					<?php comment_text(); ?>
+					</div><!-- .mt-2 -->
+				
+					<?php
+				if ( '1' == $comment->comment_approved || $show_pending_links ) {
+					comment_reply_link(
+						array_merge(
+							$args,
+							array(
+								'add_below' => 'div-comment',
+								'depth'     => $depth,
+								'max_depth' => $args['max_depth'],
+								'before'    => '<div class="reply">',
+								'after'     => '</div>',
+							)
+						)
+					);
+				}
+				?>
+				</footer><!-- .comment-meta -->
+			</article><!-- .comment-body -->
+		<?php
+	}
+}
