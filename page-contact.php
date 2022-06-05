80554cf56b1dcd950322a1db308de14d2d549f07
EmptyF-prog <gammer-gammer@mail.ru>
2022-06-06 01:14:05 +0300

    Добавление карты яндекс

--- page-contact.php
+++ page-contact.php
@@ -76,15 +76,10 @@
     </section>
     <!--  CONTACT END  -->
 
-    <!--  Google Map START  -->
-    <section id="map" class="section-padding">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 col-sm-6 col-md-3"></div>
-        </div>
-      </div>
-    </section>
-    <!--  Google Map END  -->
+
+    <?php the_content()?>
+
+
 
 
 
